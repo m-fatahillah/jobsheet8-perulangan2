91a4b8a967af7becce5b78e6caf6253dabe712f6
m-fatahillah <fatahcrown789@gmail.com>
2024-11-02 21:43:17 +0700

    tugas

--- tugas1.java
+++ tugas1.java
@@ -1,23 +1,26 @@
+import java.util.Scanner;
 public class tugas1 {
     public static void main(String[] args) {
-        int n =5;
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
         int h=1;
+
+        System.out.print("Masukkan input n : ");
+        n = sc.nextInt();
 
         while(h<=n) { 
             int i=1;
             while(i<=n) {
                 if (h==1 || h==n||i==1 || i == n)
-                System.out.print("5");
+                System.out.print(n);
                 
                 else {
                     System.out.print(" ");
                 }
                 i++;
             } 
-
             System.out.println();
             h++;
         }
-    }
-    
+    }    
 }
